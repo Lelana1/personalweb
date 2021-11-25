b3258f4589e9ea08deefde1cd59e182c4ede9aca
Wisani <matobelaw@gmail.com>
2021-11-25 13:29:58 +0200

    fixing hamburger functionality

---
+++ myscript.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-bar-list");
+    const navLink = document.querySelectorAll(".nav-bar-item");
+    
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+    
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+    
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    
+    
+    
+    
+    })
+    window.onscroll = function() {myFunction()};
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
