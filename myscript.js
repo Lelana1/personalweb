7499c2bc1cee47e3576710046b47ca674bc358a7
Wisani <matobelaw@gmail.com>
2021-11-25 14:43:18 +0200

    fixing hamburger menu

--- myscript.js
+++ myscript.js
@@ -27,3 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function myFunction() {
         if (window.pageYOffset >= sticky) {
           navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
