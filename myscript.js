324078988a402bed73db0dd98aed399caadc3905
Wisani <matobelaw@gmail.com>
2021-11-26 09:47:27 +0200

    fixing mobile view

--- myscript.js
+++ myscript.js
@@ -23,7 +23,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
     
     })
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function() 
+    {myFunction()};
+    var navbar = document.getElementById("navbar")
+    var sticky =navbar.offsetTop;
+    
     function myFunction() {
         if (window.pageYOffset >= sticky) {
           navbar.classList.add("sticky")
